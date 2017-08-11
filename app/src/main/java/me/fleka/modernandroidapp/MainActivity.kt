cc20823c1cf5392b6e2cc37911d7746a43624a17
Mladen Rakonjac <mladenbp@gmail.com>
2017-08-12 01:49:37 +0200

    Adds different build variants

--- app/src/main/java/me/fleka/modernandroidapp/MainActivity.kt
+++ app/src/main/java/me/fleka/modernandroidapp/MainActivity.kt
@@ -1,12 +1,14 @@
 package me.fleka.modernandroidapp
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        System.out.println(savedInstanceState?.getString("string"))
     }
 }
