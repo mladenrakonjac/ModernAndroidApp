0168badf777095fb589150807ef1ad9c6981b31e
Mladen Rakonjac <mladenbp@gmail.com>
2017-08-13 21:58:40 +0200

    Adds Data binding library
    Took 52 minutes

--- app/src/main/java/me/fleka/modernandroidapp/MainActivity.kt
+++ app/src/main/java/me/fleka/modernandroidapp/MainActivity.kt
@@ -1,14 +1,23 @@
 package me.fleka.modernandroidapp
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import me.fleka.modernandroidapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        System.out.println(savedInstanceState?.getString("string"))
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.apply {
+            repositoryName.text = "Medium Android Repository Article"
+            repositoryOwner.text = "Fleka"
+            numberOfStarts.text = "1000 stars"
+
+        }
     }
 }
