71fe11e44d6cc82ecf6fa6ef904cc8f3ff3c03bd
Mladen Rakonjac <mladenbp@gmail.com>
2017-08-26 01:50:34 +0200

    Refresh method and callback implementation added
    Took 1 hour 6 minutes

--- app/src/main/java/me/fleka/modernandroidapp/MainViewModel.kt
+++ app/src/main/java/me/fleka/modernandroidapp/MainViewModel.kt
@@ -1,5 +1,6 @@
 package me.fleka.modernandroidapp
 
+import me.fleka.modernandroidapp.data.OnDataReadyCallback
 import me.fleka.modernandroidapp.data.RepoModel
 
 /**
@@ -10,4 +11,13 @@ class MainViewModel {
     var text: String = ""
     var isLoading: Boolean = false
 
+    val onDataReadyCallback = object : OnDataReadyCallback {
+        override fun onDataReady(data: String) {
+            text = data
+        }
+    }
+
+    fun refresh() {
+        repoModel.refreshData(onDataReadyCallback)
+    }
 }
