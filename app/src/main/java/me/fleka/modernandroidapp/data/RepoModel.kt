494aa7b8015da672d68fd22353a2bc482066effb
Mladen Rakonjac <mladenbp@gmail.com>
2017-08-26 01:05:49 +0200

    RepoModel class added
    Took 38 minutes

---
+++ app/src/main/java/me/fleka/modernandroidapp/data/RepoModel.kt
@@ -0,0 +1,11 @@
+package me.fleka.modernandroidapp.data
+
+/**
+ * Created by Mladen Rakonjac on 8/26/17.
+ */
+class RepoModel(){
+
+    fun refreshData() : String {
+        return "Same new data"
+    }
+}
