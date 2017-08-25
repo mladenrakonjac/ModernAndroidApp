0a13d26bf5058010fed4fea3988b7c32b362b45d
Mladen Rakonjac <mladenbp@gmail.com>
2017-08-26 01:20:28 +0200

    Adds callback for RepoModel
    Took 50 minutes

--- app/src/main/java/me/fleka/modernandroidapp/data/RepoModel.kt
+++ app/src/main/java/me/fleka/modernandroidapp/data/RepoModel.kt
@@ -1,11 +1,17 @@
 package me.fleka.modernandroidapp.data
 
+import android.os.Handler
+
 /**
  * Created by Mladen Rakonjac on 8/26/17.
  */
 class RepoModel(){
 
-    fun refreshData() : String {
-        return "Same new data"
+    fun refreshData(onDataReadyCallback: OnDataReadyCallback) {
+        Handler().postDelayed({ onDataReadyCallback.onDataReady("new data") },2000)
     }
+}
+
+interface OnDataReadyCallback {
+    fun onDataReady(data : String)
 }
