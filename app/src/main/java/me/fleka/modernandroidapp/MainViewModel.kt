ec4055f0a90a75d106264d0c37b2c600a379fd21
Mladen Rakonjac <mladenbp@gmail.com>
2017-08-26 01:34:20 +0200

    Adds ViewModel for MainActivity
    Took 57 minutes

---
+++ app/src/main/java/me/fleka/modernandroidapp/MainViewModel.kt
@@ -0,0 +1,13 @@
+package me.fleka.modernandroidapp
+
+import me.fleka.modernandroidapp.data.RepoModel
+
+/**
+ * Created by Mladen Rakonjac on 8/26/17.
+ */
+class MainViewModel {
+    var repoModel: RepoModel = RepoModel()
+    var text: String = ""
+    var isLoading: Boolean = false
+
+}
