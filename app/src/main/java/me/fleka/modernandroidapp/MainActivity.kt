0b09c0d90ce4d71d2e9666618668b04625b03c81
Mladen Rakonjac <mladenbp@gmail.com>
2017-09-10 00:05:49 +0200

    MainViewModel is created and binded in MainActivity.

--- app/src/main/java/me/fleka/modernandroidapp/MainActivity.kt
+++ app/src/main/java/me/fleka/modernandroidapp/MainActivity.kt
@@ -2,27 +2,21 @@ package me.fleka.modernandroidapp
 
 import android.databinding.DataBindingUtil
 import android.os.Bundle
-import android.os.Handler
 import android.support.v7.app.AppCompatActivity
 import me.fleka.modernandroidapp.databinding.ActivityMainBinding
-import me.fleka.modernandroidapp.uimodels.Repository
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
 
-    var repository = Repository("Medium Android Repository Article",
-            "Fleka", 1000, true)
+    var mainViewModel = MainViewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.repository = repository
+        binding.viewModel = mainViewModel
         binding.executePendingBindings()
-
-        Handler().postDelayed({repository.repositoryName="New Name"
-            }, 2000)
 
     }
 }
