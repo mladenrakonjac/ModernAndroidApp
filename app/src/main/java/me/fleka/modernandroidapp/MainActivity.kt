4b620970ddca8d73c6580e171b9d6523083becc6
Mladen Rakonjac <mladenbp@gmail.com>
2017-09-17 16:02:44 +0200

    Set adapter to RecyclerView without notifyDataSetChange
    Took 9 minutes

--- app/src/main/java/me/fleka/modernandroidapp/MainActivity.kt
+++ app/src/main/java/me/fleka/modernandroidapp/MainActivity.kt
@@ -4,18 +4,29 @@ import android.arch.lifecycle.ViewModelProviders
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
 import me.fleka.modernandroidapp.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), RepositoryRecyclerViewAdapter.OnItemClickListener {
+
 
     lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        val viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        binding.viewModel = viewModel
         binding.executePendingBindings()
 
+        binding.repositoryRv.layoutManager = LinearLayoutManager(this)
+        binding.repositoryRv.adapter = RepositoryRecyclerViewAdapter(viewModel.repositories, this)
+
+    }
+
+    override fun onItemClick(position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
