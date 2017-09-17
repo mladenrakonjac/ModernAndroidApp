2e4c2ac89a43f2d85d948e86797fe3a469ea53f8
Mladen Rakonjac <mladenbp@gmail.com>
2017-09-17 13:26:48 +0200

    Adds getRepositories fun in MainViewModel
    Took 1 hour 56 minutes

--- app/src/main/java/me/fleka/modernandroidapp/MainViewModel.kt
+++ app/src/main/java/me/fleka/modernandroidapp/MainViewModel.kt
@@ -3,7 +3,9 @@ package me.fleka.modernandroidapp
 import android.arch.lifecycle.ViewModel
 import android.databinding.ObservableField
 import me.fleka.modernandroidapp.data.OnDataReadyCallback
+import me.fleka.modernandroidapp.data.OnRepositoryReadyCallback
 import me.fleka.modernandroidapp.data.RepoModel
+import me.fleka.modernandroidapp.uimodels.Repository
 
 /**
  * Created by Mladen Rakonjac on 8/26/17.
@@ -15,12 +17,24 @@ class MainViewModel : ViewModel() {
 
     val isLoading = ObservableField(false)
 
+    var repositories = ArrayList<Repository>()
+
     fun refresh(){
         isLoading.set(true)
         repoModel.refreshData(object : OnDataReadyCallback {
             override fun onDataReady(data: String) {
                 isLoading.set(false)
                 text.set(data)
+            }
+        })
+    }
+
+    fun getRepositories() {
+        isLoading.set(true)
+        repoModel.getRepositories(object : OnRepositoryReadyCallback {
+            override fun onDataReady(data: ArrayList<Repository>) {
+                isLoading.set(false)
+                repositories = data
             }
         })
     }
