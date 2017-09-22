7240982c3d73e6396a3fdc69c155c0ae546e0069
Mladen Rakonjac <mladenbp@gmail.com>
2017-09-22 02:18:22 +0200

    NetManager added
    Took 19 minutes

---
+++ app/src/main/java/me/fleka/modernandroidapp/androidmanagers/NetManager.kt
@@ -0,0 +1,17 @@
+package me.fleka.modernandroidapp.androidmanagers;
+
+import android.content.Context
+import android.net.ConnectivityManager
+
+class NetManager(private var applicationContext: Context) {
+
+    val isConnectedToInternet: Boolean?
+        get() {
+
+            val conManager = applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE)
+                    as ConnectivityManager
+
+            val ni = conManager.activeNetworkInfo
+            return ni != null && ni.isConnected
+        }
+}
