888fbce5413f94cf87ab748fad7a73963fd0fc2f
Mladen Rakonjac <mladenbp@gmail.com>
2018-02-04 22:20:11 +0100

    Set scope of NetManager as Singleton
    Took 2 hours 13 minutes

--- app/src/main/java/me/fleka/modernandroidapp/androidmanagers/NetManager.kt
+++ app/src/main/java/me/fleka/modernandroidapp/androidmanagers/NetManager.kt
@@ -3,7 +3,9 @@ package me.fleka.modernandroidapp.androidmanagers;
 import android.content.Context
 import android.net.ConnectivityManager
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class NetManager @Inject constructor(var applicationContext: Context) {
 
     val isConnectedToInternet: Boolean?
